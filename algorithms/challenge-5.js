6e22ba094f68b67ae539b10dfba30d687e1ffc47
bunmi <bunmidavids194@gmail.com>
2022-11-01 23:01:56 +0100

    Week 1 Task

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,5 +1,12 @@
-function filterList(arr) {
- 
- }
+//Implement the function filterList(arr) that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
+function filterList(arr) {
+    var newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (isNaN(arr[i]) == false && typeof(arr[i]) != "string") {
+            newArray.push(arr[i]);
+        }                               
+    }
+    return newArray;
+}
   module.exports =filterList
